53765a8370f02fd6f73d1831f445198e85e00a9d
Mehedi Hasan Sagor <mehedihasansagor.cse@gmail.com>
2024-08-18 22:11:01 +0600

    Update LaravelLivewireModalsProvider.php

--- src/Providers/LaravelLivewireModalsProvider.php
+++ src/Providers/LaravelLivewireModalsProvider.php
@@ -11,11 +11,11 @@ class LaravelLivewireModalsProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'livewire-bootstrap-modal');
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'livewire-modal');
 
         $this->publishes(
-            [__DIR__ . '/../../resources/views' => resource_path('views/vendor/livewire-bootstrap-modal')],
-            ['livewire-bootstrap-modal', 'livewire-bootstrap-modal:views']
+            [__DIR__ . '/../../resources/views' => resource_path('views/vendor/livewire-modal')],
+            ['livewire-modal', 'livewire-modal:views']
         );
 
         Livewire::component('modals', Modals::class);
