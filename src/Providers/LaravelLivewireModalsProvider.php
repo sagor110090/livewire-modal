c878a4521662922b33bbf9ce00f504a8be16480d
Mehedi Hasan Sagor <mehedihasansagor.cse@gmail.com>
2024-08-18 22:06:12 +0600

    Create LaravelLivewireModalsProvider.php

---
+++ src/Providers/LaravelLivewireModalsProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sagor110090\LivewireModal;
+
+
+use Sagor110090\LivewireModal\Components\Modals;
+use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+
+class LaravelLivewireModalsProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'livewire-bootstrap-modal');
+
+        $this->publishes(
+            [__DIR__ . '/../../resources/views' => resource_path('views/vendor/livewire-bootstrap-modal')],
+            ['livewire-bootstrap-modal', 'livewire-bootstrap-modal:views']
+        );
+
+        Livewire::component('modals', Modals::class);
+    }
+}
