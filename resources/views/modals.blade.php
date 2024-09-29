7dde1c82d2d56e9b267f991e57a8b105c57daaca
Mehedi Hasan Sagor <mehedihasansagor.cse@gmail.com>
2024-09-29 19:45:58 +0600

    Update modals.blade.php
    fixed some bug for multi modal

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1,19 +1,17 @@
 <div>
     @if ($show)
-        @teleport('body')
-            @php
-                $key = random_int(1, 100000);
-            @endphp
-        @livewire($component, $data, key($key))
-        @endteleport
+
+            @livewire($component, $data, key($component)) <!-- Use $component as a stable key -->
+
     @endif
-<!--     loader  -->
-<div  :style="`display: ${isLoading ? 'flex' : 'none'};`" style="display: none;" class="loader-container">
-    <div class="lds-ring">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+
+    <!-- Loader -->
+    <div :style="`display: ${isLoading ? 'flex' : 'none'};`" style="display: none;" class="loader-container">
+        <div class="lds-ring">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     </div>
-</div>
 </div>
